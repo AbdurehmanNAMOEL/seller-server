299f90d3ee9042dc4029ced45a330b7dd7e4d037
Abdurehman Saeed <asaeed526@gmail.com>
2022-12-03 17:08:00 +0300

    added

--- configure/db.js
+++ configure/db.js
@@ -2,7 +2,7 @@ const mongoose= require('mongoose')
 
 const connectDb = async()=>{
     try {
-       await mongoose.connect((process.env.MONGODB_URI).toString())
+       await mongoose.connect('mongodb+srv://Namoel:namoel12345@ethiogemstone.g2uxltx.mongodb.net/?retryWrites=true&w=majority')
         console.log('the Db is connected');
     } catch (error) {
         console.log(error.message)
