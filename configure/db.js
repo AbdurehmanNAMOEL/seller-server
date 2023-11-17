291ddb24c30a5cac3afaebd5ab1e7dc8cc88a737
Abdurehman Saeed <asaeed526@gmail.com>
2023-11-17 15:40:24 +0300

    user type added to the model

--- configure/db.js
+++ configure/db.js
@@ -1,13 +1,12 @@
 const mongoose= require('mongoose')
 
-const connectDb = ()=>{
+const connectDb = async()=>{
     try {
-        mongoose.connect(process.env.MONGO_URL)
+       await mongoose.connect(process.env.MONGO_URL)
         console.log('the Db is connected');
     } catch (error) {
         console.log(error.message)
     }
 }
-
 
 module.exports = connectDb
