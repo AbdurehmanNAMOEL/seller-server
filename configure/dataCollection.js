3843c7e2b59203157e3e717e5f2e553c7b534403
Abdurehman Saeed <asaeed526@gmail.com>
2023-11-17 15:23:08 +0300

    user type added to the model

--- configure/dataCollection.js
+++
@@ -1,6 +0,0 @@
-// const {MongoClient} = require('mongodb')
-// const client = new MongoClient('mongodb+srv://Namoel:namoel12345@ethiogemstone.g2uxltx.mongodb.net/?retryWrites=true&w=majority')
-// client.connect()
-// console.log("Connected")
-// module.exports = client.db('test')
-
