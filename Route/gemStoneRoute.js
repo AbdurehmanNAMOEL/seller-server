b6052e670c57dfe1c667170d13a309eb216bf71a
Abdurehman Saeed <asaeed526@gmail.com>
2023-12-29 18:45:10 +0300

    updated

--- Route/gemStoneRoute.js
+++ Route/gemStoneRoute.js
@@ -7,4 +7,5 @@ gemStoneRoute.get('/getStone',getGemStone)
 gemStoneRoute.get('/stone',getAllGemStone)
 gemStoneRoute.put('/edit/:id',protect,updateStone)
 gemStoneRoute.delete('/delete/:id',protect,deleteStone)
+
 module.exports= gemStoneRoute
